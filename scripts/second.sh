dbf585acdd381053259770d4c4e81c03c235a61b
scorpiofifth <scorpiofifth@qq.com>
2024-09-08 13:20:39 +0800

    Update

--- scripts/second.sh
+++ scripts/second.sh
@@ -22,22 +22,22 @@ echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
 # echo "CONFIG_PACKAGE_luci-app-mihomo=y" >> .config
 
 # OpenClash
-# git clone -b master https://github.com/vernesong/OpenClash.git openclash
-# mv openclash/luci-app-openclash package/
-# cd package/luci-app-openclash/tools/po2lmo
-# make && sudo make install
-# cd ../../../../
-# mkdir -p files/etc/openclash/core/
-# wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz
-# tar -zxf clash-linux-arm64.tar.gz && rm -f clash-linux-arm64.tar.gz
-# chmod 777 clash
-# mv clash files/etc/openclash/core/clash
-# wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz
-# tar -zxf clash-linux-arm64.tar.gz && rm -f clash-linux-arm64.tar.gz
-# chmod 777 clash
-# mv clash files/etc/openclash/core/clash_meta
-# wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz
-# gunzip clash-linux-arm64-2023.08.17-13-gdcc8d87.gz
-# chmod 777 clash*
-# mv clash* files/etc/openclash/core/clash_tun
-# echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
+git clone -b master https://github.com/vernesong/OpenClash.git openclash
+mv openclash/luci-app-openclash package/
+cd package/luci-app-openclash/tools/po2lmo
+make && sudo make install
+cd ../../../../
+mkdir -p files/etc/openclash/core/
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz
+tar -zxf clash-linux-arm64.tar.gz && rm -f clash-linux-arm64.tar.gz
+chmod 777 clash
+mv clash files/etc/openclash/core/clash
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz
+tar -zxf clash-linux-arm64.tar.gz && rm -f clash-linux-arm64.tar.gz
+chmod 777 clash
+mv clash files/etc/openclash/core/clash_meta
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz
+gunzip clash-linux-arm64-2023.08.17-13-gdcc8d87.gz
+chmod 777 clash*
+mv clash* files/etc/openclash/core/clash_tun
+echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config

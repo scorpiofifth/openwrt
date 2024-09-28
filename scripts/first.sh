ee93a885ea557cdbfa98082449b297d91d1773c6
ScorpioFifth <scorpiofifth@qq.com>
2024-09-28 19:53:33 +0800

    Update

--- scripts/first.sh
+++ scripts/first.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-# echo 'src-git istore https://github.com/linkease/istore;main' >> "feeds.conf.default"
+echo 'src-git istore https://github.com/linkease/istore;main' >> "feeds.conf.default"
--- scripts/second.sh
+++ scripts/second.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Power off devices
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-echo "CONFIG_PACKAGE_luci-app-poweroff=y" >> .config
-
 # Argon theme
 rm -rf package/feeds/luci/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
@@ -13,11 +9,20 @@ echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
 
 # OpenClash
-git clone -b master https://github.com/vernesong/OpenClash.git openclash
-mv openclash/luci-app-openclash package/
-cd package/luci-app-openclash/tools/po2lmo
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+pushd luci-app-openclash/tools/po2lmo
 make && sudo make install
-cd ../../../../
+popd
+cd ../..
+
+# OpenClash core
 mkdir -p files/etc/openclash/core/
 wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz
 tar -zxf clash-linux-arm64.tar.gz && rm -f clash-linux-arm64.tar.gz
@@ -33,7 +38,5 @@ chmod 777 clash*
 mv clash* files/etc/openclash/core/clash_tun
 echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
 
-# istore
-# echo "CONFIG_PACKAGE_luci-app-store=y" >> .config
-
-
+# iStore
+echo "CONFIG_PACKAGE_luci-app-store=y" >> .config
